789ff9967bfa7a2a17cc00df6e660b46197ef34d
Owen Sprague <osprague24@biomedscienceacademy.org>
2020-11-06 18:17:16 +0000

    started the assignment

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 """
 MadLibs
-Author: 
-Period/Core:
-
-
+Author: Owen Sprague
+Period/Core: 2
 """
+
+print("Time for some MadLibs!")
+print(" ")
+name1 = input("Input a name: ")
+verb1 = input("Input a verb: ")
+animal = input("Input an animal: ")
+adjective1 = input("Input an adjective: ")
+name2 = input("Input another name: ")
+adjective2 = input("Input another adjective: ")
+adjective3 = input("Input a third adjective: ")
